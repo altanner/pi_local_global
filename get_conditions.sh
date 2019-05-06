126c753a16fb3b6fa2ccb6699b855a71fd53d5b6
Al Tanner <8190834+altanner@users.noreply.github.com>
2019-05-07 00:28:59 +0100

    Update get_conditions.sh

--- get_conditions.sh
+++ get_conditions.sh
@@ -10,14 +10,14 @@ curl https://earthquaketrack.com/recent > /home/pi/weather/quakes
 
 ## parse out specifics
 CONDITIONS=$(cat /home/pi/weather/wttr_weather | awk 'NR==3' | cut -c 16-);
-TEMP=$(cat /home/pi/weather/wttr_weather | awk 'NR==4' | awk '{print $(NF-1), $NF }');
-WIND=$(cat /home/pi/weather/wttr_weather | awk 'NR==5' | awk '{print $(NF-2), $(NF-1), $NF}');
+TEMP=$(cat /home/pi/weather/wttr_weather | awk 'NR==4' | awk '{print $(NF-1), $NF }' | sed "s/°/'/" | sed "s/\.\./~/");
+WIND=$(cat /home/pi/weather/wttr_weather | awk 'NR==5' | awk '{print $(NF-1), $NF}');
 WINDDIR=$(cat /home/pi/weather/tnd_weather | grep -m 1 -oP '(?<=> from )\w+');
 PRES=$(cat /home/pi/weather/tnd_weather | grep -o -P "Pressure: </span> .{0,9}" | grep -o ".........$");
 VISI=$(cat /home/pi/weather/wttr_weather | awk 'NR==6' | awk '{print $(NF-1), $NF}');
 PRECIP=$(cat /home/pi/weather/wttr_weather | awk 'NR==7' | awk '{print $(NF-1), $NF}');
 HUM=$(cat /home/pi/weather/tnd_weather | grep -o -P "Humidity: </span> .{0,3}" | grep -o "...$");
-DEW=$(cat /home/pi/weather/tnd_weather | grep -o -P "Dew Point: </span>.{0,11}" | grep -o "..........$");
+DEW=$(cat /home/pi/weather/tnd_weather | grep -o -P "Dew Point: </span>.{0,11}" | grep -o "..........$" | sed "s/°/'/" | sed "s/<//" | sed "s/&nbsp;/ /");
 SUNRISE=$(cat /home/pi/weather/tnd_astro | grep -o -P "Sunrise Today: </span><span class=three>.{0,5}" | grep -o ".....$");
 SUNSET=$(cat /home/pi/weather/tnd_astro | grep -o -P "Sunset Today: </span><span class=three>.{0,5}" | grep -o ".....$");
 MOONPHASE=$(cat tnd_astro | grep -o -P -m 1 "cur-moon-percent.{0,40}" | sed -e 's/.*p>\(.*\)<.*/\1/');
@@ -39,9 +39,9 @@ if [[ $DEW == *"-"* ]]; then
 fi
 
 ## if temp below zero, precip is snow
-DROPLETS="Droplets";
+DROPLETS="DripDepth";
 if [[ $TEMP == *"-"* ]]; then
-    DROPLETS="Flurry flakes";
+    DROPLETS="FlurryDepth";
 fi
 
 ## if visibility is maximum at 10km, call it horizon
@@ -50,24 +50,19 @@ if [[ $VISI == "10 km" ]]; then
 fi
 
 ## create conditions file
-echo "Up see :  $CONDITIONS     " > /home/pi/weather/conditions;
-echo "Air feel :  $TEMP    " >> /home/pi/weather/conditions;
-echo "Sky move :  $WIND from $WINDDIR     " >> /home/pi/weather/conditions;
-echo "Airiness :  $PRES    " >>/home/pi/weather/conditions;
-echo "Optodist :  $VISI    " >> /home/pi/weather/conditions;
-echo "$DROPLETS :  $PRECIP    " >> /home/pi/weather/conditions;
-echo "Wettitude :  $HUM    " >> /home/pi/weather/conditions;
+echo "UpSee :  $CONDITIONS     " > /home/pi/weather/conditions;
+echo "AirFeel :  $TEMP    " >> /home/pi/weather/conditions;
+echo "SkyMove :  $WIND from $WINDDIR     " >> /home/pi/weather/conditions;
+echo "AtmoPushiness :  $PRES  " >>/home/pi/weather/conditions;
+echo "OptoDist :  $VISI     " >> /home/pi/weather/conditions;
+echo "$DROPLETS :  $PRECIP     " >> /home/pi/weather/conditions;
+echo "Wettitude :  $HUM     " >> /home/pi/weather/conditions;
 echo "$DEWPOINT :  $DEW     " >> /home/pi/weather/conditions;
-echo "Sunsoar : $SUNRISE   Sunsquirrel :  $SUNSET     " >> /home/pi/weather/conditions;
-echo "Moonloom : $MOONRISE   Moonsnooze :  $MOONSET     " >> /home/pi/weather/conditions;
+echo "SunSoar : $SUNRISE   SunSquirrel :  $SUNSET     " >> /home/pi/weather/conditions;
+echo "MoonLoom : $MOONRISE   MoonSnooze :  $MOONSET     " >> /home/pi/weather/conditions;
 echo "Lunar faction :  $MOONPHASE ($MOONPERCENT)     " >> /home/pi/weather/conditions;
-echo "Future tempest :  $STORMNAME    " >> /home/pi/weather/conditions;
-echo "Wobblemax : $QUAKEMM M, $QUAKELOC1, $QUAKELOC2     " >> /home/pi/weather/conditions;
-echo "Sizzlecity : $HOTCITY $HOTCITYTEMP 'C     " >> /home/pi/weather/conditions;
-echo "Doom sum : $CO2 ppm     " >> /home/pi/weather/conditions;
+echo "Future tempest :  $STORMNAME  " >> /home/pi/weather/conditions;
+echo "WobbleMax : $QUAKEMM M, $QUAKELOC1, $QUAKELOC2     " >> /home/pi/weather/conditions;
+echo "SizzleCity : $HOTCITY $HOTCITYTEMP 'C     " >> /home/pi/weather/conditions;
+echo "DoomSum : $CO2 ppm     " >> /home/pi/weather/conditions;
 
-sed -i "s/°/'/" /home/pi/weather/conditions;
-sed -i "s/Sky move :  ./Sky move : /" /home/pi/weather/conditions;
-sed -i "s/<//g" /home/pi/weather/conditions;
-sed -i "s/\.\./ ~ /" /home/pi/weather/conditions;
-sed -i "s/&nbsp;/ /" /home/pi/weather/conditions;
